6db2d8808a85fae30dbfa212911276b72af255cb
okidijimmy200 <okidijimmie@gmail.com>
2020-04-18 18:22:29 +0300

    completed the header section

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
 
+
+
+
 urlpatterns = [
     path('', include('books.urls')),
     path('admin/', admin.site.urls),
