6fa7edd0de017a2979140ff9ee033b5f5fd45db1
okidijimmy200 <okidijimmie@gmail.com>
2020-07-01 14:57:42 +0300

    sqlite database

--- core/settings.py
+++ core/settings.py
@@ -96,11 +96,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'library',
-        'USER':'postgres',
-        'PASSWORD':'12345',
-        'HOST':'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
